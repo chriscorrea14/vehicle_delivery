8289903a824ee9533fa0ee5d81521ea67b9af947
chriscorrea14 <chris.correa@berkeley.edu>
2018-11-28 01:59:43 -0800

    initial pass at getting the cost of a graph

--- cost.py
+++ cost.py
@@ -1,4 +1,78 @@
 # script to calculate the cost of a given graph
+from distances import distance, closest_vdc
+import pandas as pd
+import csv
+
+DELIVER_BY = pd.Timedelta(days=10)
+
+def paths(plants, dealers):
+    return plants, dealers
+
+class CostCalculatingPolicy():
+    def __init__(self):
+        # Cars waiting at various VDCs to be sent to dealers
+        self.waiting_cars = pd.DataFrame(columns=[
+            'vin', 
+            'current_loc', 
+            'next_loc', 
+            'full_path', 
+            'end_time', # when current_time == end_time, the car better be at the end
+            'leave_time' # when current_time == leave_time, the car needs to leave
+        ])
+        self.preprocess_shipping_reqs()
+
+    # We will think about a vehicle being built as "in transit" and arriving at the plant
+    # when it's done building
+    def preprocess_shipping_reqs(self):
+        reqs = pd.read_csv('data/Problem_VehicleShipmentRequirement.csv')
+        # DELETE ME.  JUST FOR TESTING PURPOSES
+        reqs = reqs.loc[0:1000]
+        next_locs, full_paths = paths(reqs['Plant'], reqs['Dealer'])
+        in_transit = pd.concat(
+            [
+                reqs['VIN'].rename('vin'),
+                next_locs.rename('next_locs'),
+                full_paths.rename('full_paths'),
+                (pd.to_datetime(reqs['Plant_Arrival_Time']) + DELIVER_BY).rename('end_time'),
+                pd.to_datetime(reqs['Plant_Arrival_Time']).rename('arrive_time')
+            ],
+            axis=1
+        )
+        self.in_transit = in_transit.sort_values(by='arrive_time')
+        self.num_cars_to_be_built = len(in_transit)
+
+    def run(self, locations):
+        current_time = in_transit.loc[next_car_built_idx, 'arrive_time']
+        cost = 0
+        while not self.waiting_cars.empty and not self.in_transit.empty:
+            if self.waiting_cars.empty:
+                cost += self.process_in_transit()
+            elif self.in_transit.empty:
+                cost += self.process_waiting_cars()
+            else:
+                next_in_transit = self.in_transit.loc[0]
+                next_waiting_car = self.waiting_cars.loc[0]
+                if next_in_transit['arrive_time'] < next_waiting_car['leave_time']:
+                    cost += self.process_in_transit()
+                else:
+                    cost += self.process_waiting_cars()
+        return cost
+
+    # AKA the car is arriving at it's next location
+    def process_in_transit(self, car_in_transit):
+        # if you reach vdc capacity, then process those
+        next_in_transit = self.in_transit.loc[0]
+
+    # AKA the car needs to leave now for the next spot.  will take any cars with it
+    def process_waiting_cars(self):
+        next_waiting_car = self.waiting_cars.loc[0]
+        same_path = self.waiting_cars[ # cars that can go with the next waiting car
+            (self.waiting_cars['current_loc'] == next_waiting_car['current_loc']) & 
+            (self.waiting_cars['next_loc'] == next_waiting_car['next_loc'])
+        ]
+
 
 if __name__ == '__main__':
-	pass
+    locations = pd.read_excel(io='data/location.xlsx', sheet_name='LocationLatLong')
+    locations = locations.set_index('Location')
+    run(locations)
--- distances.py
+++ distances.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 from math import sin, cos, radians, sqrt, asin
 
-def distance(table, location1, location2):
+def distance(locations_table, location1, location2):
     # Defined on bottom of page 14 of Intro to Informs
     if isinstance(location1, str) and location1.isdigit():
         location1 = int(location1)
     if isinstance(location2, str) and location2.isdigit():
         location2 = int(location2)
 
-    row1, row2 = table.loc[location1], table.loc[location2]
+    row1, row2 = locations_table.loc[location1], locations_table.loc[location2]
     lat1, long1 = radians(row1['Latitude']), radians(row1['Longitude'])
     lat2, long2 = radians(row2['Latitude']), radians(row2['Longitude'])
 
@@ -17,6 +17,9 @@ def distance(table, location1, location2):
       sin((lat1 - lat2)/2.0)**2 + cos(lat1) * cos(lat2) * sin((long1 - long2)/2.0)**2
     ))
     return 1.2 * 3959 * c
+
+def closest_vdc(locations_table, dealer):
+    print locations_table.head(20)
 
 if __name__ == '__main__':
     locations = pd.read_excel(io='data/location.xlsx', sheet_name='LocationLatLong')
