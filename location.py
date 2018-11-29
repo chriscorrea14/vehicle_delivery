1ec90888cd3d7dcb539928a4b315229770889dd2
Arthur Chen <aulchen@berkeley.edu>
2018-11-29 14:20:16 -0800

    Fixed typoes

--- location.py
+++ location.py
@@ -7,31 +7,41 @@ class Location:
         self.lat = lat
         self.lon = lon
 
-    def isVDB(self):
+    def isVDC(self):
         return False
 
     def getLat(self):
         return self.lat
+
     def getLon(self):
         return self.lon
+
     def setLat(self, lat):
         self.lat = lat
+
     def setLon(self, lon):
         self.lon = lon
 
 class VDC(Location):
-    cap, rail = 0. False
+    cap = 0
+    rail = False
 
     def __init__(self, lat, lon, cap, rail):
         super().__init__(self, lat, lon)
         self.cap = cap
         self.rail = rail
 
+    def isVDC(self):
+        return True;
+
     def getCap(self):
         return self.cap
+
     def getRail(self):
         return self.rail
+
     def setCap(self, cap):
         self.cap = cap
+
     def setRail(self, rail):
         self.rail = rail
