6487ba084166009bcd5e19ccc21e0b829aa5ad75
Arthur Chen <aulchen@berkeley.edu>
2018-12-02 14:11:24 -0800

    Cleaned up reader code

---
+++ csvreader.py
@@ -0,0 +1,15 @@
+from location import *
+
+def readData():
+    locdict = {}
+    with open("LocationLatLong.csv", newline = '', encoding='utf-8') as csvfile:
+        locreader = csv.reader(csvfile, dialect='excel', delimiter=',')
+        for row in locreader:
+            locdict[str(row[0])] = Location(name = str(row[0]), lat = float(row[2]), lon = float(row[3]))
+    with open("ExistingVDC.csv", newline = '', encoding='utf-8') as csvfile:
+        vdcreader = csv.reader(csvfile, dialect = 'excel', delimiter = ',')
+        for row in vdcreader:
+            locname = str(row[0])
+            loc = locdict[locname]
+            locdict[locname] = loc.toVDC(cap = float(row[1]), rail = bool(row[2]))
+    return locdict
--- csvtest.py
+++ csvtest.py
@@ -1,23 +1,4 @@
-from location import *
-import pandas as pd
-import numpy as np
-from math import sin, cos, radians, sqrt, asin
+from csvreader import *
 
-locdict = {}
-
-def readLocations():
-    with open("LocationLatLong.csv", newline = '', encoding='utf-8') as csvfile:
-        locreader = csv.reader(csvfile, dialect='excel', delimiter=',')
-        for row in locreader:
-            locdict[str(row[0])] = Location(str(row[0]), float(row[2]), float(row[3]))
-
-def readVDC():
-    with open("ExistingVDC.csv", newline = '', encoding='utf-8') as csvfile:
-        vdcreader = csv.reader(csvfile, dialect = 'excel', delimiter = ',')
-        for row in vdcreader:
-            locname = str(row[0])
-            loc = locdict[locname]
-            locdict[locname] = loc.toVDC(cap = float(row[1]), rail = bool(row[2]))
-
-readLocations()
-readVDC()
+locdict = readData()
+print(locdict["3A"])
