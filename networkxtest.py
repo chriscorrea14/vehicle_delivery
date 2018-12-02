f62eea9d92a275f48db6977182e1117b7653e582
Arthur Chen <aulchen@berkeley.edu>
2018-12-02 14:39:50 -0800

    Simple graph test completed

--- networkxtest.py
+++ networkxtest.py
@@ -1,12 +1,6 @@
-from location import *
-import pandas as pd
-import numpy as np
-from math import sin, cos, radians, sqrt, asin
-
-locdict = {}
-locdict["Los Angeles"] = Location("Los Angeles", 34.05, -118.25)
-locdict["New York"] = Location("New York", 40.7128, -74.0060)
-
+from csvreader import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def distance(location1, location2):
     lat1, lat2 = location1.getLat(), location2.getLat()
@@ -17,6 +11,26 @@ def distance(location1, location2):
     ))
     return 1.2 * 3959 * c
 
-print(locdict["Los Angeles"])
-print(locdict["New York"])
-print(distance(locdict["Los Angeles"], locdict["New York"]) / 1.2)
+#locdict = readData()
+
+locdict = {}
+locdict["Los Angeles"] = Location("Los Angeles", 34.05, -118.25)
+locdict["New York"] = Location("New York", 40.7128, -74.0060)
+locdict["London"] = Location("London", 51.5074, -0.1278)
+locdict["Tokyo"] = Location("Tokyo", 35.6895, 139.6917)
+
+
+
+G = nx.Graph()
+G.add_nodes_from(locdict.keys())
+
+for loc1 in locdict.keys():
+    for loc2 in locdict.keys():
+        G.add_edge(loc1, loc2, weight = distance(locdict[loc1], locdict[loc2]))
+
+print(G.edges.data('weight'))
+'''
+plt.subplot(121)
+nx.draw(G, with_labels=True, font_weight='bold')
+plt.show()
+'''
